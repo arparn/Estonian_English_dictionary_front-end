c15abc76cefea0c0deb1fa7e22c61b298e9ada98
Artur-Aleksander Pärnoja <arthurp200148@gmail.com>
2021-05-16 11:50:28 +0300

    Add delete estonian words and translations functionality

--- src/app/delete-estonian/delete-estonian.component.ts
+++ src/app/delete-estonian/delete-estonian.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {EnglishWord} from "../models/english-word";
+import {DeleteEnglishService} from "../delete-english/delete-english.service";
+import {DeleteEstonianService} from "./delete-estonian.service";
 
 @Component({
   selector: 'app-delete-estonian',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeleteEstonianComponent implements OnInit {
 
-  constructor() { }
+  allWords: EnglishWord[];
+  englishWords: EnglishWord[];
+
+  constructor(private deleteEstonianService: DeleteEstonianService) { }
 
   ngOnInit(): void {
+    this.getAll();
+  }
+
+  getAll(): void {
+    this.deleteEstonianService.getAll().subscribe( response => this.allWords = response);
+  }
+
+  deleteWord(id: number): void {
+    this.deleteEstonianService.deleteWord(id).subscribe(response => {
+      this.allWords = response;
+      this.englishWords = [];
+    });
+  }
+
+  deleteTranslation(word_id: number, translation_id: number): void {
+    this.deleteEstonianService.deleteTranslation(word_id, translation_id).subscribe(response => {
+      this.allWords = response;
+      this.englishWords = [];
+    });
+  }
+
+  getWord(word: string): void {
+    this.deleteEstonianService.getEnglishTranslation(word).subscribe((response) => {
+      if (response != null) {
+        this.englishWords = response['wordList']
+      } else {
+        this.englishWords = [];
+      }
+    });
   }
 
 }
--- src/app/delete-estonian/delete-estonian.service.ts
+++ src/app/delete-estonian/delete-estonian.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {EnglishWord} from "../models/english-word";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DeleteEstonianService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
+
+  getAll(): Observable<EnglishWord[]>{
+    const url = 'api/estonian-english';
+    return this.http.get<EnglishWord[]>(url);
+  }
+
+  deleteWord(id: number): Observable<EnglishWord[]> {
+    const url = `api/estonian-english/${id}`;
+    return this.http.delete<EnglishWord[]>(url, this.httpOptions);
+  }
+
+  deleteTranslation(word_id: number, translation_id: number): Observable<EnglishWord[]>{
+    const url = `api/estonian-english/${word_id}/${translation_id}`;
+    return this.http.delete<EnglishWord[]>(url, this.httpOptions);
+  }
+
+  getEnglishTranslation(word: string): Observable<JSON> {
+    const url = `api/estonian-english/translate?word=${word}`;
+    return this.http.get<JSON>(url);
+  }
 }
